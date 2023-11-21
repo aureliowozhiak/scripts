736473d00b08361e2dc367258bcbc2bf691cb17a
Marcos Aurelio Wozhiak Junior de Oliveira <aureliowozhiak@gmail.com>
2023-11-21 13:30:01 -0300

    Create turnon-turnof.sh

---
+++ turnon-turnof.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+
+
+# Check the current status of the EC2 instance
+
+status=$(aws ec2 describe-instances --instance-ids EC2-INSTANCE-ID --query 'Reservations[0].Instances[0].State.Name' --output text)
+
+
+
+# Parse the output to extract the instance status
+
+case $status in
+
+    running)
+
+        echo "The instance is already running."
+
+        # Turn off the instance
+
+        aws ec2 stop-instances --instance-ids EC2-INSTANCE-ID
+
+        echo "The instance has been turned off."
+
+        ;;
+
+    stopped)
+
+        # Turn on the instance
+
+        aws ec2 start-instances --instance-ids EC2-INSTANCE-ID
+
+        echo "The instance has been turned on."
+
+        #wait for the instance to start
+
+        aws ec2 wait instance-running --instance-ids EC2-INSTANCE-ID
+
+        echo "The instance is now running."
+
+        # return the IP address of the instance
+
+        aws ec2 describe-instances --instance-ids EC2-INSTANCE-ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text
+
+        ;;
+
+    pending|stopping)
+
+        echo "The instance is currently transitioning and cannot be turned on."
+
+        ;;
+
+    terminated)
+
+        echo "The instance no longer exists."
+
+        ;;
+
+    *)
+
+        echo "The instance is in an unknown state."
+
+        ;;
+
+esac
+
+#!/bin/bash
+
+
+
+# Check the current status of the EC2 instance
+
+status=$(aws ec2 describe-instances --instance-ids EC2-INSTANCE-ID --query 'Reservations[0].Instances[0].State.Name' --output text)
+
+
+
+# Parse the output to extract the instance status
+
+case $status in
+
+    running)
+
+        echo "The instance is already running."
+
+        # Turn off the instance
+
+        aws ec2 stop-instances --instance-ids EC2-INSTANCE-ID
+
+        echo "The instance has been turned off."
+
+        ;;
+
+    stopped)
+
+        # Turn on the instance
+
+        aws ec2 start-instances --instance-ids EC2-INSTANCE-ID
+
+        echo "The instance has been turned on."
+
+        #wait for the instance to start
+
+        aws ec2 wait instance-running --instance-ids EC2-INSTANCE-ID
+
+        echo "The instance is now running."
+
+        # return the IP address of the instance
+
+        aws ec2 describe-instances --instance-ids EC2-INSTANCE-ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text
+
+        ;;
+
+    pending|stopping)
+
+        echo "The instance is currently transitioning and cannot be turned on."
+
+        ;;
+
+    terminated)
+
+        echo "The instance no longer exists."
+
+        ;;
+
+    *)
+
+        echo "The instance is in an unknown state."
+
+        ;;
+
+esac
+
